d571f491cf4ddb4fbd689546096fb8fc1d2971cb
Mithilesh <mithtechx@gmail.com>
2026-08-20 18:25:00 +0530

    Render AcademicModule on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+'use client';
+
+import AcademicModule from '@/components/AcademicModule';
+
 export default function Home() {
-  return null;
+  return (
+    <main className="min-h-screen bg-slate-50 p-4 sm:p-8">
+      <div className="max-w-5xl mx-auto">
+        <AcademicModule />
+      </div>
+    </main>
+  );
 }
